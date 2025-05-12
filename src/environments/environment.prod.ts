f6dc217bb196cb7ae5eb21b43838b84138bfd61d
Mikołaj Gramowski <gramosmiki@onet.pl>
2025-05-12 21:35:16 +0200

    config: add WebSocket URL to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://your-production-api-url', // Replace with your actual production API URL
+  wsUrl: 'ws://your-production-api-url', // Replace with your actual production WebSocket URL
 };
